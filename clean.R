65f000d9a665d9719f2b8f27f9fa311dd1380119
Carlos Cámara <carlos.camara@warwick.ac.uk>
2021-06-16 12:32:49 +0100

    Attempt to generate a simple dashboard from github actions.

--- clean.R
+++ clean.R
@@ -3,3 +3,8 @@ library(wfenexus)
 url_kindergartens <- "slupsk/data-raw/kindergartens.json"
 
 ci_get_data(url_kindergartens, "kindergartens.csv")
+
+
+knitr::knit("slupsk/test_dashboard.Rmd")
+
+print("Script successfully run!")
